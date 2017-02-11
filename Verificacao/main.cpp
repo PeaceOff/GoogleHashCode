ebef71ba4efa12d8e5094c9e6de5ec4f1ce6d750
PeaceOff <davidazevedo1996@hotmail.com>
2017-02-11 20:09:35 +0000

    97%

--- Verificacao/main.cpp
+++ Verificacao/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -18,14 +19,38 @@ void debugInput(int rows, int  cols, int  minIng, int maxCells,vector<vector<cha
 	return;
 }
 
+bool checkSlice(int xi, int yi, int xf, int yf, vector<vector<char>>* pizza, vector<vector<bool>>* checkVec) {
+	
+	//Return true if valid
+	int tomatoeCount = 0;
+	int mushroomCount = 0;
+	for (int x = xi; x <= xf; x++){
+		for (int y = yi; y <= yf; y++) {
+			if ((*checkVec)[x][y]) {
+				return false;
+			}
+			(*checkVec)[x][y] = true;
+			if ((*pizza)[x][y] == 'T') {
+				tomatoeCount++;
+			}
+			else {
+				mushroomCount++;
+			}
+		}
+	};
+
+	return true;
+}
+
 int main() {
 	ifstream inputFile;
 	ifstream outputFile;
 	string iFileName;
 	string oFileName;
 
-	//Input data
+	//Input data---------------------------------------------------------------------------------------
 	vector<vector<char>> pizza;
+	vector<vector<bool>> checkVec;
 	int rows, cols, minIng, maxCells;
 
 	cout << "Input File [Same directory with extension]: ";
@@ -33,7 +58,7 @@ int main() {
 	
 	cout << "Opening File..." << endl;
 	inputFile.open(iFileName);
-	
+
 	if (inputFile.is_open()) {
 		//Read Integers
 		cout << "Reading Data..." << endl;
@@ -42,13 +67,27 @@ int main() {
 		inputFile >> minIng;
 		inputFile >> maxCells;
 
+		//Limpar o resto da primeira linha
+		string lixo;
+		getline(inputFile, lixo);
+
 		//Read Pizza
 		for (int x = 0; x < rows; x++) {
-			char* line = (char*)malloc(sizeof(char) * cols);
-			inputFile.getline(line,cols);
+			
+			string line;
+			getline(inputFile,line);
+			vector<char> l;
+			vector<bool> ll;
+			
 			for (int y = 0; y < cols; y++) {
-				pizza[x][y] = line[y];
+			
+				l.push_back(line[y]);
+				ll.push_back(false);
+			
 			}
+
+			pizza.push_back(l);
+			checkVec.push_back(ll);
 		}
 	}
 	else {
@@ -61,10 +100,58 @@ int main() {
 
 	cout << "Data Read, Closing File..." << endl;
 	inputFile.close();
-	
-	//todo
 
-	//Output data
+	//Output data-------------------------------------------------------------------------------
+	cout << "Output File [Same directory with extension]: ";
+	cin >> oFileName;
 
+	cout << "Opening File..." << endl;
+	outputFile.open(oFileName);
+
+	int nSlices, nOccupied;
+
+	if (outputFile.is_open()) {
+		//Read Integers
+		cout << "Reading Data..." << endl;
+		outputFile >> nSlices;
+		cout << "NSlices : " << nSlices << endl;
+
+		//Limpar o resto da primeira linha
+		string lixo;
+		getline(outputFile, lixo);
+
+		stringstream ss;
+		string line;
+
+		while (nSlices) {
+			//Read Slice
+			line.clear();
+			getline(outputFile, line);
+			ss.clear();
+			ss << line;
+			int xi, yi, xf, yf;
+			ss >> xi >> yi >> xf >> yf;
+			cout << "Read : " << xi << " " << yi << " " << xf << " " << yf << endl;
+
+			if (!checkSlice(xi, yi, xf, yf, &pizza, &checkVec)) {
+				
+				cout << "Invalid Result! at " << xi << ";" << yi << ";" << xf << ";" << yf << endl;
+				outputFile.close();
+				system("pause");
+				return -1;
+
+			};
+			nSlices--;
+		}
+	}
+	else {
+		cout << "Could not read file!" << endl;
+		system("pause");
+		return -1;
+	}
+
+	cout << "Data Read, Closing File..." << endl;
+	outputFile.close();
+	system("pause");
 	return 0;
 }
